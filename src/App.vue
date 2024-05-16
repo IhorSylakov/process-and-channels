1b7d80a62bd9b4c36e091407d2f03e1f85e215f0
sylakov <108350582+IhorSylakov@users.noreply.github.com>
2024-05-16 18:04:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="app">
+    <ul class="process-list">
+      <li class="process-item">
+        <input
+          type="number"
+          min="1"
+          max="10"
+          v-model.number="channelsCount"
+          @input="updateChannelsCount"
+          @keydown="preventOutOfRangeInput"
+        >
+      </li>
+      <li
+        v-for="process in processList"
+        :key="process.id"
+        :class="['process-item', {'is-active': activeProcess === process.name}]"
+        @click="setProcessType(process.name)"
+      >
+          <img
+            height="60"
+            width="75"
+            :alt="process.name"
+            :src="require(`./assets/${process.name}.svg`)"
+          >
+      </li>
+    </ul>
+    <MainComponent
+      :processType="activeProcess"
+      :channels="channelsList"
+    />
+  </div>
+</template>
+
+<script>
+  import MainComponent from './components/MainComponent.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      MainComponent
+    },
+    data() {
+      return {
+        processList: [
+          {
+            id: 1,
+            name: 'ongoing',
+          },
+          {
+            id: 3,
+            name: 'linear',
+          },
+          {
+            id: 2,
+            name: 'non-linear',
+          },
+          {
+            id: 3,
+            name: 'bidirectional',
+          },
+        ],
+        channelsCount: 3,
+        activeProcess: 'ongoing'
+      }
+    },
+    computed: {
+      channelsList() {
+        const array = [];
+        for (let i = 1; i <= this.channelsCount; i++) {
+          array.push({
+            id: i,
+            text: `test${i}`,
+          });
+        }
+
+        return array;
+      }
+    },
+    methods: {
+      setProcessType(processType) {
+        this.activeProcess = processType;
+      },
+      updateChannelsCount(event) {
+        this.channelsCount = event.target.value;
+      },
+      preventOutOfRangeInput(event) {
+        const inputValue = parseInt(event.target.value);
+        if (inputValue < 1) {
+          this.channelsCount = 1;
+        } else if (inputValue > 10) {
+          this.channelsCount = 10;
+        }
+      },
+    }
+}
+</script>
+
+<style lang="scss">
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  :root {
+    --system-theme: enable;
+
+    container-name: html;
+  }
+
+  body {
+    --theme-color: #000;
+    --theme-bg-color: #fff;
+
+    color: var(--theme-color);
+    background: var(--theme-bg-color);
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    @container html style(--system-theme: enable) {
+      @media (prefers-color-scheme: light) {
+        --theme-color: #000;
+        --theme-bg-color: #fff;
+      }
+
+      @media (prefers-color-scheme: dark) {
+        --theme-color: #fff;
+        --theme-bg-color: #000;
+      }
+    }
+  }
+
+  .process-list {
+    margin: 0 0 20px;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    gap: 15px;
+  }
+
+  .process-item {
+    padding: 10px;
+    min-width: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid var(--theme-color);
+
+    &.is-active {
+      background: lightgreen;
+    }
+
+    input {
+      font-size: 24px;
+    }
+  }
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,567 @@
+<template>
+  <div
+    class="container"
+    :style="processType !== 'ongoing' ? 'min-height: 320px;' : ''"
+  >
+    <div
+      class="process-and-channels"
+      :style="styles"
+    >
+      <div
+        :class="['process', `process--${processType}`]"
+        :style="processType === 'non-linear' ? nonLinearStyles(containerWidth) : ''"
+      >
+        <div
+          v-for="channel in channels"
+          :key="channel.id"
+          :class="[ 'channel', `channel--${processType}` ]"
+          :style="processType === 'ongoing' ? ongoingStyles(channel.id) : ''"
+        >
+          <div class="channel__item">
+            <div class="channel__item-icon">
+              <img
+                height="32"
+                width="32"
+                alt="channel type"
+                src="../assets/blank-channel.svg"
+              >
+            </div>
+          </div>
+
+          <div
+            class="channel__item-text"
+            :style="processType === 'ongoing' ? position(channel.id) : ''"
+          >
+            <div contenteditable="true">{{ channel.text }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="resizer-x"
+      @mousedown="startDragX"
+      @mousemove="doDragX"
+      @mouseup="stopDragX"
+    />
+    <div
+      v-if="processType === 'ongoing'"
+      class="resizer-y"
+      @mousedown="startDragY"
+      @mousemove="doDragY"
+      @mouseup="stopDragY"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MainComponent',
+    props: {
+      processType: {
+        type: String,
+        default: 'ongoing',
+      },
+      channels: {
+        type: Array,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        isDragging: false,
+        startX: 0,
+        startY: 0,
+        startWidth: 0,
+        startHeight: 0,
+        diametr: 200,
+        containerWidth: 558,
+        containerMinWidth: 300,
+        containerMinHeight: 200,
+      };
+    },
+    computed: {
+      channelsCount() {
+        // + 1 because arrow also take place on process line and affects calculations
+        return this.channels.length + 1;
+      },
+      styles() {
+        const width = `width: ${this.containerWidth}px;`;
+        const computedStyles = `--min-width: ${this.containerWidth}px;`;
+        const channelsCount = `--channels-count: ${this.channels.length};`;
+        const radius = `--circle-radius: ${this.diametr / 2}px;`;
+
+        return width + computedStyles + channelsCount + radius;
+      },
+    },
+    methods: {
+      ongoingChannelDegree(id) {
+        const itemsCount = this.channelsCount;
+        const angle = 360 / itemsCount;
+        const degree = angle * id - 90;
+
+        return degree;
+      },
+
+      position(id) {
+        const degree = this.ongoingChannelDegree(id);
+        const temp = degree < 0 ? 360 + degree : degree;
+        const posVertical = temp < 181 ? 'top: 100%' : 'bottom: 100%';
+        let posHorizontal = 'left: -5%';
+
+        if (degree > 90) {
+          posHorizontal = 'right: 25%';
+        } else if (degree < 90) {
+          posHorizontal = 'left: 25%';
+        }
+
+        return `${posVertical}; ${posHorizontal};`;
+      },
+
+      ongoingStyles(id) {
+        const degree = this.ongoingChannelDegree(id);
+        const sin = Math.sin(degree * (Math.PI / 180)).toFixed(4);
+        const cos = Math.cos(degree * (Math.PI / 180)).toFixed(4);
+
+        return `--sin: ${sin}; --cos: ${cos};`;
+      },
+
+      nonLinearStyles(fullWidth) {
+        const itemHeight = 120;
+        const sideIndents = 150;
+        const minWidth = 50;
+        const maxWidth = 150;
+        const computedWidth = (fullWidth - sideIndents) / this.channelsCount;
+        let itemWidth = computedWidth;
+
+        if (computedWidth < minWidth) {
+          itemWidth = minWidth;
+        } else if (computedWidth > maxWidth) {
+          itemWidth = maxWidth;
+        }
+
+        // Pythagorean Theorema to find hypotenuse
+        const hypotenuse = Math.sqrt(itemHeight ** 2 + itemWidth ** 2);
+
+        // https://en.wikipedia.org/wiki/Right_triangle
+        // required angle of right triangle is arctan of ratio width to height,
+        // but Math.atan() return in radians, so we need convert it
+        const degree = Math.atan(itemWidth / itemHeight) / (Math.PI / 180);
+
+        return `--height: ${hypotenuse}px; --degree: ${degree}deg; --item-width: ${itemWidth}px;`;
+      },
+
+      startDragX(event) {
+        this.isDragging = true;
+        this.startWidth = this.containerWidth;
+        this.startX = event.clientX;
+        document.addEventListener('mousemove', this.doDragX);
+        document.addEventListener('mouseup', this.stopDragX);
+      },
+      doDragX(event) {
+        if (this.isDragging) {
+          const deltaX = event.clientX - this.startX;
+          this.containerWidth = Math.max(this.containerMinWidth, this.startWidth + deltaX);
+        }
+      },
+      stopDragX() {
+        this.isDragging = false;
+        document.removeEventListener('mousemove', this.doDragX);
+        document.removeEventListener('mouseup', this.stopDragX);
+      },
+
+      startDragY(event) {
+        this.isDragging = true;
+        this.startHeight = this.diametr + 110;
+        this.startY = event.clientY;
+        document.addEventListener('mousemove', this.doDragY);
+        document.addEventListener('mouseup', this.stopDragY);
+      },
+      doDragY(event) {
+        if (this.isDragging) {
+          const deltaY = event.clientY - this.startY;
+          this.diametr = Math.max(this.containerMinHeight, this.startHeight + deltaY) - 110;
+        }
+      },
+      stopDragY() {
+        this.isDragging = false;
+        document.removeEventListener('mousemove', this.doDragY);
+        document.removeEventListener('mouseup', this.stopDragY);
+      },
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  .container {
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    border: 1px solid var(--theme-color);
+  }
+
+  .resizer-x {
+    position: absolute;
+    bottom: -1px;
+    right: -17px;
+    top: -1px;
+    display: flex;
+    cursor: ew-resize;
+    background-color: #bbb;
+    user-select: none;
+
+    &::after {
+      content: '=';
+      display: block;
+      writing-mode: vertical-lr;
+      text-align: center;
+      line-height: 16px;
+      font-size: 24px;
+    }
+  }
+
+  .resizer-y {
+    position: absolute;
+    bottom: -17px;
+    right: -1px;
+    left: -1px;
+    display: flex;
+    justify-content: center;
+    cursor: ns-resize;
+    background-color: #bbb;
+    user-select: none;
+
+    &::after {
+      content: '=';
+      display: block;
+      line-height: 16px;
+      font-size: 24px;
+    }
+  }
+
+  .process-and-channels {
+    --stroke-width: 8px;
+    --side-paddings: 75px;
+    --ongoing-koef: 0.8;
+    --arrowSize: 32px;
+    --channel-icon-size: 32px;
+
+    padding: 40px var(--side-paddings) 10px;
+    justify-content: center;
+    overflow: hidden;
+  }
+
+  .process {
+    &::after {
+      content: '';
+      position: absolute;
+      height: var(--arrowSize);
+      width: var(--arrowSize);
+      border: solid #c4c4c4;
+      border-width: var(--stroke-width) var(--stroke-width) 0 0;
+    }
+  }
+
+  .channel {
+    &__item {
+      position: relative;
+      z-index: 1;
+    }
+
+    &__item-icon {
+      width: var(--channel-icon-size);
+      height: var(--channel-icon-size);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      font-size: 20px;
+      font-weight: bold;
+      border-radius: 50%;
+      background: #fff;
+    }
+
+    &__item-text {
+      position: absolute;
+      margin: 5px 0;
+      z-index: 1;
+      max-height: 40px;
+      width: 110%;
+      min-width: 50px;
+      max-width: 175px;
+      line-height: 20px;
+      overflow: hidden;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      font-size: 16px;
+      font-weight: 400;
+      color: #000;
+
+      [contenteditable="true"],
+      [contenteditable] {
+        background: rgba(#fff, 0.9);
+        padding: 0 2px;
+        user-select: initial;
+
+        &:focus,
+        &:hover {
+          background: rgba(#fff, 0.6);
+        }
+      }
+    }
+  }
+
+  .process--linear {
+    align-self: center;
+    display: flex;
+    position: relative;
+    width: 100%;
+    height: var(--stroke-width);
+    min-width: calc(20px + 80px * var(--channels-count));
+    padding-right: 10px;
+    margin: 32px 0 62px;
+    background: #c4c4c4;
+    border-radius: 4px;
+
+    &::after {
+      right: 0;
+      top: calc(-0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+      transform: rotate(45deg);
+    }
+  }
+
+  .channel--linear {
+    position: relative;
+    width: calc(100% / var(--channels-count));
+
+    .channel__item {
+      margin-top: calc(-0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+    }
+
+    .channel__item-text {
+      width: 100%;
+      transform: translateX(calc(-50% + var(--channel-icon-size) / 2));
+    }
+
+    &:first-child:last-child {
+      margin-inline: auto;
+      max-width: 50%;
+
+      .channel__item-icon {
+        margin-inline: auto;
+      }
+
+      .channel__item-text {
+        left: 50%;
+        transform: translateX(-50%);
+      }
+    }
+
+    &:nth-child(even) .channel__item-text {
+      bottom: calc(100% + 0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+    }
+
+    &:nth-child(odd) .channel__item-text {
+      top: calc(100% + 0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+    }
+  }
+
+  .process--bidirectional {
+    align-self: center;
+    display: flex;
+    position: relative;
+    width: 100%;
+    height: var(--stroke-width);
+    min-width: calc(20px + 80px * var(--channels-count));
+    padding: 0 10px;
+    margin: 32px 0 62px;
+    background: #c4c4c4;
+    border-radius: 4px;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: var(--arrowSize);
+      width: var(--arrowSize);
+      border: solid #c4c4c4;
+      border-width: var(--stroke-width) var(--stroke-width) 0 0;
+      left: -5px;
+      top: calc(-0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+      transform: rotate(225deg);
+    }
+
+    &::after {
+      right: -5px;
+      top: calc(-0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+      transform: rotate(45deg);
+    }
+  }
+
+  .channel--bidirectional {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    width: calc(100% / var(--channels-count));
+
+    .channel__item {
+      margin-top: calc(-0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+    }
+
+    .channel__item-text {
+      width: 100%;
+    }
+
+    &:first-child:last-child {
+      margin-inline: auto;
+      max-width: 90%;
+
+      .channel__item-text {
+        transform: none;
+      }
+    }
+
+    &:nth-child(even) .channel__item-text {
+      bottom: calc(100% + 0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+    }
+
+    &:nth-child(odd) .channel__item-text {
+      top: calc(100% + 0.5 * (var(--channel-icon-size) - var(--stroke-width)));
+    }
+  }
+
+  .process--ongoing {
+    position: relative;
+    width: 80%;
+    min-width: calc(((var(--min-width) - var(--side-paddings) * 2) * var(--ongoing-koef)));
+    height: calc(var(--circle-radius) * 2);
+    margin: 10px auto 50px;
+
+    &::before {
+      border: var(--stroke-width) solid #c4c4c4;
+      border-radius: 100%;
+      content: '';
+      min-width: calc(((var(--min-width) - var(--side-paddings) * 2) * var(--ongoing-koef)));
+      height: calc(var(--circle-radius) * 2);
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    &::after {
+      --sinArrow: -1;
+      --cosArrow: 0;
+      --xr: calc(((var(--min-width) - var(--side-paddings) * 2) * var(--ongoing-koef)) / 2);
+      --y1: calc(var(--sinArrow) * (var(--circle-radius) - var(--stroke-width)));
+      --x1: calc(var(--cosArrow) * (var(--xr) - var(--stroke-width)));
+
+      left: 50%;
+      top: 50%;
+      margin-top: calc(-1 * (var(--arrowSize) + var(--stroke-width)) / 2);
+      margin-left: calc(-1 * (var(--arrowSize) + var(--stroke-width)) / 2);
+      transform: translateX(var(--x1)) translateY(var(--y1)) rotate(45deg);
+    }
+  }
+
+  .channel--ongoing {
+    --xr: calc(((var(--min-width) - var(--side-paddings) * 2) * var(--ongoing-koef)) / 2);
+    --y1: calc(var(--sin) * (var(--circle-radius) - var(--stroke-width)));
+    --x1: calc(var(--cos) * (var(--xr) - var(--stroke-width)));
+
+    position: absolute;
+    width: 20%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    left: 50%;
+    top: 50%;
+    margin-top: -16px;
+    margin-left: -10%;
+    transform: translateX(var(--x1)) translateY(var(--y1));
+  }
+
+  .process--non-linear {
+    display: flex;
+    height: 120px;
+    position: relative;
+    padding-right: var(--item-width);
+    width: calc(var(--item-width) * (var(--channels-count) + 1));
+    margin: 30px auto 60px;
+
+    &::before {
+      border-radius: var(--stroke-width);
+      content: '';
+      width: var(--stroke-width);
+      height: var(--height);
+      background: #c4c4c4;
+      transform: rotate(calc(-1 * var(--degree)));
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      transform-origin: right bottom;
+    }
+
+    &::after {
+      right: 0;
+      bottom: 0;
+      margin-bottom: -6px;
+      transform: rotate(calc(45deg - var(--degree)));
+      transform-origin: right bottom;
+      border-width: 0 var(--stroke-width) var(--stroke-width) 0;
+    }
+  }
+
+  .channel--non-linear {
+    position: relative;
+    height: 100%;
+    width: var(--item-width);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .channel__item {
+      left: 50%;
+      margin: calc(-1 * var(--stroke-width)) 0 0;
+    }
+
+    .channel__item-text {
+      left: 45%;
+      bottom: calc(100% + var(--stroke-width));
+    }
+
+    &::before {
+      border-radius: var(--stroke-width);
+      content: '';
+      width: var(--stroke-width);
+      height: var(--height);
+      background: #c4c4c4;
+      transform: rotate(var(--degree));
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      transform-origin: bottom left;
+    }
+
+    &:nth-last-child(2n) {
+      justify-content: flex-end;
+
+      .channel__item {
+        margin: 0 0 -16px;
+      }
+
+      .channel__item-text {
+        bottom: auto;
+        top: calc(100% + 16px);
+      }
+
+      &::before {
+        left: auto;
+        right: 0;
+        bottom: 0;
+        transform: rotate(calc(-1 * var(--degree)));
+        transform-origin: right bottom;
+      }
+    }
+  }
+</style>
